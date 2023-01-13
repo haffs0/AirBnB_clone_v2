d69371d1e5e6bde87e29ed806ed7c400e14153f3
Abu Afeez <abuafeezabolarinwa@yahoo.com>
2023-01-13 10:16:49 +0100

    add web flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Start a flask application
+Your web application must be listening on 0.0.0.0, port 5000
+Routes:
+    /: display "Hello HBNB!"
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Displays 'Hello HBNB!'"""
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
